eaf97bb10fbaa2a41e371d596671f505d1191969
ChamMat <mathieu.chamoulaud@gmail.Com>
2020-07-31 22:09:42 +0200

    erreur d'exportation de tables

--- backOffice/migrations/Version20200711204443.php
+++ backOffice/migrations/Version20200711204443.php
@@ -20,7 +20,8 @@ final class Version20200711204443 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE mode (id INT AUTO_INCREMENT NOT NULL, date_id INT DEFAULT NULL, mode VARCHAR(255) NOT NULL, INDEX IDX_97CA47ABB897366B (date_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE mode (id INT AUTO_INCREMENT NOT NULL, mode VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        // $this->addSql('CREATE TABLE mode (id INT AUTO_INCREMENT NOT NULL, date_id INT DEFAULT NULL, mode VARCHAR(255) NOT NULL, INDEX IDX_97CA47ABB897366B (date_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE mode ADD CONSTRAINT FK_97CA47ABB897366B FOREIGN KEY (date_id) REFERENCES date (id)');
         $this->addSql('ALTER TABLE date DROP mode');
     }
